b7b74cbe13e99dbdd725f928c47421ed3def4d5a
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2025-01-16 23:50:25 +0900

    ルーティングを整理

--- routes/web.php
+++ routes/web.php
@@ -21,43 +21,28 @@ Route::get('/', function () {
 Route::middleware('guest')->group(function () {
     Route::get('login', [AuthenticatedSessionController::class, 'create'])->name('login');
 });
-Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return Inertia::render('Dashboard');
-    })->name('dashboard');
-});
 
 //初期ステータスの登録
 Route::middleware('auth')->group(function () {
     Route::get('/registration', [StatusController::class, 'create'])->name('create');
     Route::post('/confirm', [StatusController::class, 'store'])->name('store');
-});
-
-//目標の登録
-Route::middleware('auth')->group(function () {
+    //目標の登録
     Route::get('/goal-registration', [SavingController::class, 'create'])->name('Saving_Goal_Create');
     Route::post('/goal-confirm', [SavingController::class, 'store'])->name('goal-confirm');
-});
-
-//ユーザーページ
-Route::get('/userpage', [StatusController::class, 'index'])
-    ->middleware(['auth', 'verified'])
-    ->name('Saving_List');
-
-Route::middleware('auth')->group(function () {
+    //ユーザーページ
+    Route::get('/userpage', [StatusController::class, 'index'])
+    ->middleware(['auth', 'verified'])->name('Saving_List');
+    //詳細ぺーじ
+    Route::get('/{id}', [SavingController::class, 'show'])->name('saving.show');
+    Route::post('/update', [HistoryController::class, 'store'])->name('update');
+    //元々あるやつ
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-//詳細ぺーじ
-Route::middleware('auth')->group(function () {
-    Route::get('/{id}', [SavingController::class, 'show'])->name('saving.show');
-    Route::post('/update', [HistoryController::class, 'store'])->name('update');
+    Route::get('/dashboard', fn() => Inertia::render('Dashboard'))->name('dashboard');
 });
 
 // Route::get('/dashboard', function () {
 //     return Inertia::render('Dashboard');
 // })->middleware(['auth', 'verified'])->name('dashboard');
-
 require __DIR__.'/auth.php';
