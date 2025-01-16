24b57bc4c968925a1145950ab124d49558bd1baf
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2025-01-17 02:56:53 +0900

    URL変更

--- routes/web.php
+++ routes/web.php
@@ -18,37 +18,37 @@ Route::get('/', function () {
     ]);
 });
 
-Route::get('./login', function () {
+Route::get('/login', function () {
     return Inertia::render('Auth.Login', [AuthenticatedSessionController::class, 'create'])->name('login');
 });
-Route::get('./register', function () {
+Route::get('/register', function () {
     return Inertia::render('Auth.Register', [AuthenticatedSessionController::class, 'store'])->name('register');
 });
 
 
-Route::get('./dashboard', function () {
+Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('./profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('./profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('./profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
 //初期ステータスの登録
 Route::middleware('auth')->group(function () {
-    Route::get('./registration', [StatusController::class, 'create'])->name('create');
-    Route::post('./confirm', [StatusController::class, 'store'])->name('store');
+    Route::get('/registration', [StatusController::class, 'create'])->name('create');
+    Route::post('/confirm', [StatusController::class, 'store'])->name('store');
     //目標の登録
-    Route::get('./goal-registration', [SavingController::class, 'create'])->name('Saving_Goal_Create');
-    Route::post('./goal-confirm', [SavingController::class, 'store'])->name('goal-confirm');
+    Route::get('/goal-registration', [SavingController::class, 'create'])->name('Saving_Goal_Create');
+    Route::post('/goal-confirm', [SavingController::class, 'store'])->name('goal-confirm');
     //ユーザーページ
-    Route::get('./userpage', [StatusController::class, 'index'])
+    Route::get('/userpage', [StatusController::class, 'index'])
     ->middleware(['auth', 'verified'])->name('Saving_List');
     //詳細ぺーじ
-    Route::get('./{id}', [SavingController::class, 'show'])->name('saving.show');
-    Route::post('./update', [HistoryController::class, 'store'])->name('update');
+    Route::get('/{id}', [SavingController::class, 'show'])->name('saving.show');
+    Route::post('/update', [HistoryController::class, 'store'])->name('update');
 
 });
 
