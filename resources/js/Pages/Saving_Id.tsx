f87850fef6e946a51e07c03908ee03b475f362f9
ayasato2019 <50820335+ayasato2019@users.noreply.github.com>
2025-01-17 09:45:36 +0900

    初回登録時の確認

--- resources/js/Pages/Saving_Id.tsx
+++ resources/js/Pages/Saving_Id.tsx
@@ -39,8 +39,21 @@ export default function SavingId() {
 
     if (!savings) {
         return (
-            <div>
-                <p>データが見つかりません。</p>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+                <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                    <div className="flex items-center justify-center gap-2 w-full h-screen">
+                        <Link
+                            href='./registration'
+                            className='flex items-center justify-center rounded-md border border-transparent bg-gradation min-w-32 max-w-52 px-4 py-2 max-h-10 text-sm font-bold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gradation-blue focus:bg-gradation focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-gradation'>
+                            初回登録した？
+                        </Link>
+                        <Link
+                            href='./userpage'
+                            className='flex items-center justify-center rounded-md border border-transparent bg-gradation min-w-32 max-w-52 px-4 py-2 max-h-10 text-sm font-bold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gradation-blue focus:bg-gradation focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-gradation'>
+                            ユーザーページ
+                        </Link>
+                    </div>
+                </div>
             </div>
         );
     }
@@ -106,13 +119,13 @@ export default function SavingId() {
     const userExtravagance: number = status.extravagance;
     const userDonation: number = status.donation;
 
-// 積立額の計算
-const calculateTotalSavings = (savingsId: number, histories: HistoryTypes[], goalGroupId: number): number => {
-    return histories
-        .filter((history) => history.goal_group_id === savingsId) // goal_group_id でフィルタリング
-        .map((history) => parseFloat(String(history.amount_saved))) // amount_saved を数値に変換
-        .reduce((total, amount) => total + amount, 0); // 合計を計算
-};
+    // 積立額の計算
+    const calculateTotalSavings = (savingsId: number, histories: HistoryTypes[], goalGroupId: number): number => {
+        return histories
+            .filter((history) => history.goal_group_id === savingsId) // goal_group_id でフィルタリング
+            .map((history) => parseFloat(String(history.amount_saved))) // amount_saved を数値に変換
+            .reduce((total, amount) => total + amount, 0); // 合計を計算
+    };
 
     // 必要なstateとuseEffectの配置
     const [currentPrice, setCurrentPrice] = useState<number>(0);
@@ -190,10 +203,10 @@ const calculateTotalSavings = (savingsId: number, histories: HistoryTypes[], goa
                             <div>
                                 <p className='text-sm font-semibold'>{today}</p>
                                 {/* {saving.goal_level === 1 ? ( */}
-                                    <TitleSavings
-                                        type={1}
-                                        title={saving.goal_name}
-                                    />
+                                <TitleSavings
+                                    type={1}
+                                    title={saving.goal_name}
+                                />
                                 {/* // ) : ''} */}
                                 <form action="./update" method='POST'>
                                     <input type="hidden" name="_token" value={csrfToken.current} />
@@ -227,10 +240,10 @@ const calculateTotalSavings = (savingsId: number, histories: HistoryTypes[], goa
                                     />
                                 </form>
                                 <Link
-                                href='./userpage'
-                                className='flex items-center justify-center rounded-md border border-transparent bg-gradation min-w-32 max-w-52 mt-10 mx-auto px-4 py-2 max-h-10 text-sm font-bold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gradation-blue focus:bg-gradation focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-gradation'>
-                                ユーザーページ
-                            </Link>
+                                    href='./userpage'
+                                    className='flex items-center justify-center rounded-md border border-transparent bg-gradation min-w-32 max-w-52 mt-10 mx-auto px-4 py-2 max-h-10 text-sm font-bold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gradation-blue focus:bg-gradation focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-gradation'>
+                                    ユーザーページ
+                                </Link>
                             </div>
                         </li>
                     </ul>
--- resources/js/Pages/Saving_List.tsx
+++ resources/js/Pages/Saving_List.tsx
@@ -33,9 +33,22 @@ export default function Saving_List() {
 
     if (!status || !savings || !histories) {
         return (
-            <div>
-                <p>ステータスが見つかりません。</p>
+            <div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div className="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                <div className="flex items-center justify-center gap-2 w-full h-screen">
+                    <Link
+                        href='./registration'
+                        className='flex items-center justify-center rounded-md border border-transparent bg-gradation min-w-32 max-w-52 px-4 py-2 max-h-10 text-sm font-bold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gradation-blue focus:bg-gradation focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-gradation'>
+                        初回登録した？
+                    </Link>
+                    <Link
+                        href='./userpage'
+                        className='flex items-center justify-center rounded-md border border-transparent bg-gradation min-w-32 max-w-52 px-4 py-2 max-h-10 text-sm font-bold uppercase tracking-widest text-white transition duration-150 ease-in-out hover:bg-gradation-blue focus:bg-gradation focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2 active:bg-gradation'>
+                        ユーザーページ
+                    </Link>
+                </div>
             </div>
+        </div>
         );
     }
 
